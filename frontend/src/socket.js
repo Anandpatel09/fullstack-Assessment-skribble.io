aa3b5be77ec118aaf599631b1675c2851b9d3e45
Anandpatel09 <anandbca2021@gmail.com>
2026-05-28 11:09:58 +0530

    some changes

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -1,8 +1,18 @@
+// import { io } from 'socket.io-client';
+
+// const defaultServerUrl = import.meta.env.VITE_SERVER_URL || `${window.location.protocol}//${window.location.hostname}:4000`;
+
+// export const socket = io(defaultServerUrl, {
+//   reconnectionAttempts: 5,
+//   timeout: 5000
+// });
 import { io } from 'socket.io-client';
 
-const defaultServerUrl = import.meta.env.VITE_SERVER_URL || `${window.location.protocol}//${window.location.hostname}:4000`;
+const serverUrl =
+  import.meta.env.VITE_API_URL ||
+  'https://anands-skribble-io.onrender.com';
 
-export const socket = io(defaultServerUrl, {
+export const socket = io(serverUrl, {
   reconnectionAttempts: 5,
   timeout: 5000
 });
